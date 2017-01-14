bd547a5d715b22dab4a69fb1f0935b61fd0c4836
Zen Kyprianou <zen@kyprianou.eu>
2017-01-14 17:52:57 +0000

    add config path to configuration

--- lib/endpointer/configuration.rb
+++ lib/endpointer/configuration.rb
@@ -1,11 +1,18 @@
 module Endpointer
   class Configuration
 
-    attr_accessor :invalidate, :cache_dir
+    attr_accessor :invalidate, :cache_dir, :resource_config
+
+    DEFAULT_CONFIG_PATH = './endpointer.json'
 
     def initialize(invalidate = nil, cache_dir = nil)
       @invalidate = invalidate || false
       @cache_dir = cache_dir || File.join(Dir.tmpdir, "endpointer_cache")
+      begin
+        @resource_config = File.read(DEFAULT_CONFIG_PATH)
+      rescue Errno::ENOENT
+        @resource_config = nil
+      end
     end
   end
 end
--- spec/endpointer/configuration_spec.rb
+++ spec/endpointer/configuration_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
 describe Endpointer::Configuration do
+  let(:some_config_file) { 'some config' }
+  let(:config_file_path) { './endpointer.json' }
+
+  before do
+    File.write(config_file_path, some_config_file)
+  end
+
   describe '.new' do
     it 'initializes to the defaults' do
       options = described_class.new
       expect(options.invalidate).to be_falsey
       expect(options.cache_dir).to eq(File.join(Dir.tmpdir, "endpointer_cache"))
+      expect(options.resource_config).to eq(some_config_file)
+    end
+
+    context 'the default config doesnt exist' do
+      before do
+        FileUtils.remove_entry(config_file_path)
+      end
+
+      it 'sets the config to nil' do
+        options = described_class.new
+        expect(options.resource_config).to be_nil
+      end
     end
 
     context 'when initialized with arguments' do
